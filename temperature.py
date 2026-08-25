29831da121a8cd2ac0eca02ca1afe84c34d84266
Insharah Irshad <insharahirshad314@gmail.com>
2026-08-25 09:54:27 -0700

    Complete Module 1 AI and Python fundamentals

---
+++ temperature.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("\n--- Temperature Converter ---")
+print("Celsius:", celsius)
+print("Fahrenheit:", fahrenheit)
